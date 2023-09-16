1ea53e10768c4b638a2a3293982a3e39243bfe9b
adiMallya <adimallya99@gmail.com>
2023-09-16 19:12:07 +0530

    fix: dropdown menu prop type

--- src/documentation/components/Menu/index.tsx
+++ src/documentation/components/Menu/index.tsx
@@ -9,11 +9,11 @@ export const Menu = (): JSX.Element => {
   const [drop, setDrop] = useState<boolean>(false);
 
   return (
-    <S.MenuContainer aria-label="menu" drop={drop}>
+    <S.MenuContainer aria-label="menu" dropdown={drop ? 'drop' : 'collapse'}>
       <S.Dropdown aria-label="dropdown" onClick={() => setDrop(!drop)}>
         <h3 aria-label="menu-title">Components</h3>
         <span className="dropdown-arrow">
-          <S.DropdownIcon icon={faChevronDown} role="button" drop={drop} />
+          <S.DropdownIcon icon={faChevronDown} role="button" dropdown={drop ? 'drop' : 'collapse'} />
         </span>
       </S.Dropdown>
       <S.MenuLinks aria-label="doc-navigation">
