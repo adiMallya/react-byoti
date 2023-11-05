27056970a8523074baa4c874405e81670d53ca18
adiMallya <adimallya99@gmail.com>
2023-11-06 01:49:23 +0530

    add: prop types for Badges

---
+++ src/components/Badge/Badge.types.ts
@@ -0,0 +1,13 @@
+import { HtmlHTMLAttributes } from "react";
+
+type HexValue = string;
+
+export interface BadgeProps extends HtmlHTMLAttributes<HTMLSpanElement> {
+    color?: HexValue,
+    fontSize?: number,
+    variant?: 'solid' | 'outline'
+}
+
+export interface AvatarBadgeProps extends BadgeProps {
+    status?: 'online' | 'offline' | 'busy' | 'away' | 'count'
+}
