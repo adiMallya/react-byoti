a0cc1d553564f02e619883e87feb0eca2c276564
adiMallya <adimallya99@gmail.com>
2023-11-08 12:24:43 +0530

    feat: Avatar status badges

--- src/pages/docs/Badge.doc.tsx
+++ src/pages/docs/Badge.doc.tsx
@@ -18,8 +18,9 @@ export const BadgeDoc = (): JSX.Element => {
         <p>{doc.description}</p>
       </S.Section>
       <S.Section>
-        {doc.subsection.map(
-          ({ title, description, keywords, examples, code }) => (
+        {doc.subsection
+          .slice(0, -1)
+          .map(({ title, description, keywords, examples, code }) => (
             <>
               {title && <h3>{title}</h3>}
               <p>
@@ -35,15 +36,46 @@ export const BadgeDoc = (): JSX.Element => {
                 )}
               </p>
               <S.ComponentDisplay>
-                {examples.map(({ text, ...props}, index) => (
-                  <Badge {...props} key={index}>{text}</Badge> 
+                {examples.map(({ text, ...props }, index) => (
+                  <Badge {...props} key={index}>
+                    {text}
+                  </Badge>
                 ))}
               </S.ComponentDisplay>
               <h4>Usage</h4>
               <S.Code>{parse(code)}</S.Code>
             </>
-          )
-        )}
+          ))}
+      </S.Section>
+      <S.Section>
+        {doc.subsection
+          .slice(-1)
+          .map(({ title, description, keywords, examples, code }) => (
+            <>
+              {title && <h3>{title}</h3>}
+              <p>
+                {parse(
+                  description
+                    .split(" ")
+                    .map((word) =>
+                      keywords.includes(word)
+                        ? `<span aria-label="component-props">${word}</span>`
+                        : word
+                    )
+                    .join(" ")
+                )}
+              </p>
+              <S.ComponentDisplay>
+                {examples.map(({ status, size, color, ...props }, index) => (
+                  <Avatar {...props} key={index} size={size}>
+                    <AvatarBadge status={status} size={size} color={color} />
+                  </Avatar>
+                ))}
+              </S.ComponentDisplay>
+              <h4>Usage</h4>
+              <S.Code>{parse(code)}</S.Code>
+            </>
+          ))}
       </S.Section>
     </DocLayout>
   );
