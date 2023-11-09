e757816f05840e64c9d7b2bf1460d572e023ed60
adiMallya <adimallya99@gmail.com>
2023-11-09 18:28:56 +0530

    feat: Heading component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 
-import { Home, AlertDoc, AvatarDoc, BadgeDoc, ButtonDoc } from "src/pages";
+import { Home, AlertDoc, AvatarDoc, BadgeDoc, ButtonDoc, HeadingDoc } from "src/pages";
 import { useTheme } from "src/context/ThemeContext";
 import { GlobalStyles, lightTheme, darkTheme } from "./styles";
 
@@ -17,6 +17,7 @@ function App(): JSX.Element {
         <Route path="/docs/avatar" element={<AvatarDoc/>}/>
         <Route path="/docs/badge" element={<BadgeDoc/>} />
         <Route path="/docs/button" element={<ButtonDoc />} />
+        <Route path="/docs/heading" element={<HeadingDoc/>} />
       </Routes>
     </ThemeProvider>
   );
